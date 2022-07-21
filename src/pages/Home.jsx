655285ed933f0557d77a08464f55531bb6784b3c
mmdseq <mmdseq@gmail.com>
2022-07-22 03:19:07 +0430

    ready to deploy

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserResults from '../component/users/UserResults'
+import UserSearch from '../component/users/UserSearch'
+
+const Home = () => {
+  return (
+    <>
+      <UserSearch />
+      <UserResults />
+    </>
+  )
+}
+
+export default Home
